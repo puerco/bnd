f0f245bab4ddb6cbeef09b8e589ec9a520bbbfb3
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-04-14 16:44:46 -0600

    Drop jsonl funcs from bundle tool
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- pkg/bundle/tool.go
+++ pkg/bundle/tool.go
@@ -4,14 +4,9 @@
 package bundle
 
 import (
-	"bufio"
-	"bytes"
 	"errors"
 	"fmt"
 	"io"
-	"os"
-	"path/filepath"
-	"strings"
 
 	// "github.com/secure-systems-lab/go-securesystemslib/dsse"
 	protobundle "github.com/sigstore/protobuf-specs/gen/pb-go/bundle/v1"
@@ -121,60 +116,4 @@ func (t *Tool) ExtractAttestationJSON(bundle *protobundle.Bundle) ([]byte, error
 	}
 
 	return dssePayload.DsseEnvelope.Payload, nil
-}
-
-// FlattenJSONDirectoryToWriter flattens all JSON files in a directory
-func (t *Tool) FlattenJSONDirectoryToWriter(path string, w io.Writer) error {
-	dirContents, err := os.ReadDir(path)
-	if err != nil {
-		return fmt.Errorf("opening dir: %w", err)
-	}
-
-	for _, entry := range dirContents {
-		if entry.IsDir() {
-			continue
-		}
-
-		if !strings.HasSuffix(entry.Name(), ".json") {
-			continue
-		}
-
-		f, err := os.Open(filepath.Join(path, entry.Name()))
-		if err != nil {
-			return fmt.Errorf("opening file: %w", err)
-		}
-		defer f.Close()
-
-		if _, err := io.Copy(w, t.FlattenJSONStream(f)); err != nil {
-			return fmt.Errorf("writing stream")
-		}
-		if _, err := w.Write([]byte("\n")); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// FlattenJSON flattens a JSON document into a single line, suitable to add to
-// a jsonl file.
-func (t *Tool) FlattenJSONStream(r io.Reader) io.Reader {
-	newInput := ""
-	scanner := bufio.NewScanner(r)
-	buf := make([]byte, 0, 64*1024)
-	// Increase the buf max value to 10 mb just in case
-	// we encounter huge lines
-	scanner.Buffer(buf, 1024*1024*10)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line == "" {
-			continue
-		}
-		newInput += line + " "
-	}
-	return strings.NewReader(newInput)
-}
-
-func (t *Tool) FlattenJSON(data []byte) ([]byte, error) {
-	r := t.FlattenJSONStream(bytes.NewReader(data))
-	return io.ReadAll(r)
 }
